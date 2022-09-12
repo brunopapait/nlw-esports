781e4a5b5c81641c3ef544663b45c39edde4ebb5
brunopapait <brunohpapait@gmail.com>
2022-09-12 20:13:00 -0300

    feat: add games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,35 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="Logo da aplicação" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/game-1.png" alt="Game 1" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="Game 2" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="Game 3" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="Game 4" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="Game 5" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="Game 6" />
+        </a>
+      </div>
     </div>
   );
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-      }
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 2.08%, #43E7AD 64%, #E1D55D 50.57%)',
+      },
     },
   },
   plugins: [],
