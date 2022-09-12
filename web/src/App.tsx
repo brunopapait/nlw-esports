bba468d8098405c39d3860dd786bb5199fd1f1bb
brunopapait <brunohpapait@gmail.com>
2022-09-12 19:59:47 -0300

    feat: add logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
 import "./styles/main.css";
+import logo from "./assets/logo-nlw-esports.svg";
 
 function App() {
-  return <div className="w-8 h-8 bg-black dark:bg-slate-500">Oii</div>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="Logo da aplicação" />
+    </div>
+  );
 }
 
 export default App;
