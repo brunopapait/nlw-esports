c1a36a7b4216cf2bf3dda0e064e9b358c7624e78
brunopapait <brunohpapait@gmail.com>
2022-09-12 22:54:06 -0300

    feat: alter colors footer

--- mobile/src/theme/index.ts
+++ mobile/src/theme/index.ts
@@ -15,7 +15,7 @@ export const THEME = {
     SUCCESS: '#34D399',
     ALERT: '#F87171',
 
-    FOOTER: ['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)'],
+    FOOTER: ['rgba(0,0,0,0)', 'rgba(0,0,0,0.1)', 'rgba(0,0,0,0.2)', 'rgba(0,0,0,0.3)', 'rgba(0,0,0,0.4)', 'rgba(0,0,0,0.5)', 'rgba(0,0,0,0.6)', 'rgba(0,0,0,0.7)', 'rgba(0,0,0,0.8)', 'rgba(0,0,0,0.9)', 'rgba(0,0,0,1)'],
     OVERLAY: 'rgba(0,0,0,0.6)',
   },
 
