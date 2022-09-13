507d72a1519df82ac8ef43e5c75b0b1d78d396a1
brunopapait <brunohpapait@gmail.com>
2022-09-12 22:55:16 -0300

    feat: create show list to games

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,9 @@
-import { View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
@@ -12,6 +15,15 @@ export function Home() {
       <Heading
         title="Encontre o seu duo !"
         subtitle="Selecione o game que deseja jogar .."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
