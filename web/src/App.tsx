b4fe0685c70832d41cc9231742d50c5d6ad7c6f5
brunopapait <brunohpapait@gmail.com>
2022-09-12 21:41:41 -0300

    feat: implemets landing page web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import "./styles/main.css";
 import logo from "./assets/logo-nlw-esports.svg";
+import { MagnifyingGlassPlus } from "phosphor-react";
 
 function App() {
   return (
@@ -15,24 +16,79 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-1.png" alt="Game 1" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-2.png" alt="Game 2" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-3.png" alt="Game 3" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              Counter Strike
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-4.png" alt="Game 4" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-5.png" alt="Game 5" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a className="relative rounded-lg overflow-hidden" href="">
           <img src="/game-6.png" alt="Game 6" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou o seu duo ?
+            </strong>
+            <span className="text-zinc-400">
+              Publique um anúncio e para encontrar novos players !
+            </span>
+          </div>
+
+          <button
+            className="py-3 px-4 bg-violet-500 text-white rounded-md hover:bg-violet-600 flex flex-items-center gap-2"
+            type="button"
+          >
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,10 +2,14 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 2.08%, #43E7AD 64%, #E1D55D 50.57%)',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 3.08%, #43E7AD 33.94%, #E1D55D 90.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
