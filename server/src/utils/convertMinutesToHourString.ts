8d8c4d667e42010f66c9327523948527a75051ab
brunopapait <brunohpapait@gmail.com>
2022-09-14 00:17:12 -0300

    feat: refactor function

--- server/src/utils/convertMinutesToHourString.ts
+++ server/src/utils/convertMinutesToHourString.ts
@@ -7,5 +7,8 @@ export function convertMinutesToHourString(minutes: number) {
   const hours = Math.floor(minutes / 60);
   const minutesLeft = minutes % 60;
 
-  return `${hours}:${minutesLeft}`;
+  const formattedHours = String(hours).padStart(2, "0");
+  const formattedMinutes = String(minutesLeft).padStart(2, "0");
+
+  return `${formattedHours}:${formattedMinutes}`;
 }
