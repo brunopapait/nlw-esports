2062b8415ff1b31ecf35ecfb2025bf2f15710ba4
brunopapait <brunohpapait@gmail.com>
2022-09-14 22:28:10 -0300

    up

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import { View, Image, FlatList } from "react-native";
 import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-
-import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.1.102:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={logoImg} />
@@ -19,7 +26,7 @@ export function Home() {
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
